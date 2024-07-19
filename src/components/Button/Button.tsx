c3d325fab7b651ff02efa08ef63258110c0548ae
Yunus <theyuunus@gmail.com>
2024-07-19 16:53:15 +0500

    #fix #tsx #css #storybook added Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,27 +1,60 @@
 import React from "react";
 import "./Button.css";
+import classNames from "classnames";
 
 export interface ButtonProps {
     children?: React.ReactNode;
     className?: string;
-    onClick?: () => void;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
     type?: "button" | "submit" | "reset";
-    ButtonType?: "secondary" | "primary";
+    buttonType?: "primary" | "secondary" | "success";
+    borderRadius?: number;
+    fontFamily?: string;
+    backgroundColor?: string;
+    color?: string;
+    paddingY?: number;
+    paddingX?: number;
+    fontSize?: number
 }
 
 const Button: React.FC<ButtonProps> = ({
     children,
-    className,
+    className = "",
     onClick,
-    type,
-    ButtonType
+    type = "button",
+    buttonType = "primary",
+    borderRadius = 10,
+    fontFamily = "sans-serif",
+    backgroundColor,
+    color = "#fff",
+    paddingY = 12,
+    paddingX = 30,
+    fontSize = 16
 }) => {
-    const buttonClass = ButtonType === "primary" ? "button-primary" : "button-secondary";
+    const buttonClass = classNames("button", {
+        "button-primary": buttonType === "primary",
+        "button-secondary": buttonType === "secondary",
+        "button-success": buttonType === "success",
+    }, className);
+
+    const buttonStyle: React.CSSProperties = {
+        borderRadius: `${borderRadius}px`,
+        fontFamily: fontFamily,
+        backgroundColor: backgroundColor,
+        color: color,
+        paddingTop: `${paddingY}px`,
+        paddingBottom: `${paddingY}px`,
+        paddingLeft: `${paddingX}px`,
+        paddingRight: `${paddingX}px`,
+        fontSize: fontSize
+    };
+
     return (
         <button
-            className={`button ${buttonClass} ${className}`}
+            className={buttonClass}
             onClick={onClick}
-            type={type || "button"}
+            type={type}
+            style={buttonStyle}
         >
             {children}
         </button>
